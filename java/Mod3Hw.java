b3c8135be4dc6f0c7e8a13f749b343b12d74469c
Abdu-w <61803409+Abdu-w@users.noreply.github.com>
2020-05-12 04:19:22 -0400

    Update Mod3Hw.java

--- java/Mod3Hw.java
+++ java/Mod3Hw.java
@@ -1,6 +1,8 @@
-public class Part3 {
-    public static void main(String []args){
+public class Mod3 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
         multiply(10,100);
+        inchToMeters(1);
     }
 
     public static float multiply(float a, float b){
@@ -8,20 +10,12 @@ public class Part3 {
         System.out.println(Product);
         return  Product;
     }
-}
-////////////////////////////////////////////////
-
-public class Part4 {
-    public static void main(String []args){
-        inchToMeters(1);
-
-    }
-
-    public static double inchToMeters(double a){
+      public static double inchToMeters(double a){
         double conversionFactor = 39.37;
         double meters= a / conversionFactor;
         System.out.println(meters);
         return meters;
     }
+
 
 }
